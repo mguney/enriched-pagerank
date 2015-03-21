cb7341368858b44fe65749e4221ae85378734b39
Mustafa Guney <mguney.cs@gmail.com>
2015-03-21 19:56:41 +0200

    htmldaotest

---
+++ enriched-pagerank-core/src/test/java/com/gun3y/pagerank/dao/HtmlTitleDaoTest.java
@@ -0,0 +1,39 @@
+package com.gun3y.pagerank.dao;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.gun3y.pagerank.utils.HibernateUtils;
+
+public class HtmlTitleDaoTest {
+
+    static HtmlTitleDao htmlTitleDao;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        htmlTitleDao = new HtmlTitleDao();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        HibernateUtils.getSessionFactory().close();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        Assert.assertTrue(htmlTitleDao.count() > -1);
+    }
+
+}
